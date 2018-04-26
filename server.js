65cb2513cdf66dc9f833200442383298dcb871dd
Ozair Khan <ozairsaddress@gmail.com>
2018-04-25 22:11:56 -0600

    Testing routing

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'))
 require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+require("app/routing/htmlRoutes.js")(app);
 
 
 
