b5275f0d5183b6f1e66c55c7ba208aa2bbf95265
Ozair Khan <ozairsaddress@gmail.com>
2018-04-25 22:07:12 -0600

    Something

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var morgan  = require('morgan')
 
 var app = express();
 var PORT = process.env.PORT || 8080; 
@@ -7,7 +8,7 @@ var PORT = process.env.PORT || 8080;
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+app.use(morgan('dev'))
 require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
